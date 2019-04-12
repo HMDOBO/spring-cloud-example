ede3e1f7f472f3dcd95e707d318c2c6eebb7ecdc
admin <zcjswing@163.com>
2019-04-12 19:12:04 +0800

    spring cloud config 注册服务到eureka注册中心

--- spring-cloud-config-example/spring-cloud-config-client/src/main/java/com/springcloud/config/client/ConfigClientApplication.java
+++ spring-cloud-config-example/spring-cloud-config-client/src/main/java/com/springcloud/config/client/ConfigClientApplication.java
@@ -3,21 +3,24 @@ package com.springcloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-    @Value("${from}")
-    private String info;
+    @Value("${info.from}")
+    String info;
 
-    @GetMapping("info")
+    @RequestMapping(value = "/info")
     public String info() {
         return info;
     }
--- spring-cloud-config-example/spring-cloud-config-server/src/main/java/com/springcloud/config/server/ConfigServerApplication.java
+++ spring-cloud-config-example/spring-cloud-config-server/src/main/java/com/springcloud/config/server/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.springcloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer // 开启spring-cloud-config服务端功能
+@EnableDiscoveryClient // 将配置中心服务端注册到eureka注册中心
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
